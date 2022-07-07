427d10571f69d7fab17e48e967d59268a8c28c1f
andony09 <jef_ando@hotmail.com>
2022-07-07 14:35:10 -0500

    Create main.py

---
+++ clase7/main.py
@@ -0,0 +1,13 @@
+import operaciones as o
+class main:
+    sum= o.suma(100,10,50)
+    res= o.resta(10,5)
+    multi= o.multi(2,8)
+    divi= o.div(10,2)
+    print("el resultado es ",sum)
+    print("el resultado es ",res)
+    print("el resultado es ",res)
+    print("el resultado es ",sum)
+
+if __name__:"__main__"
+main()
